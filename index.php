b5b361daf25c65badfe63b289c120b90e507c707
Frederic.de <frederic.delval@gmail.com>
2018-03-26 11:18:09 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: FredericD
+ * Date: 25-03-18
+ * Time: 13:11
+ */
+require "vendor/autoload.php";
+use \App\Agent;
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-COMPATIBLE" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="assets/plugins/font-awesome/css/fontawesome.min.css" rel="stylesheet" />
+
+</head>
+<body>
+<?php
+
+
+?>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="/assets/plugins/jquery/jquery-3.3.1.min.js"></script>
+<script src="/assets/plugins/popper/popper.min.js"></script>
+<script src="/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
